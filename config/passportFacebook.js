c945feebaa5634ad5600b012344d5224171ade6a
Samuel Jimoh <jimohsamolumuyiwa@gmail.com>
2024-04-24 21:15:22 +0100

    strategy commit

--- config/passportFacebook.js
+++ config/passportFacebook.js
@@ -1,18 +1,18 @@
-const FacebookStrategy = require('passport-facebook');
-const User = require('../model/users');
+// const FacebookStrategy = require('passport-facebook');
+// const User = require('../model/users');
 
-const passportFacebook = new FacebookStrategy(
-  {
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK
-  },
+// const passportFacebook = new FacebookStrategy(
+//   {
+//     clientID: process.env.FACEBOOK_APP_ID,
+//     clientSecret: process.env.FACEBOOK_APP_SECRET,
+//     callbackURL: process.env.FACEBOOK_CALLBACK
+//   },
 
-  (accessToken, refreshToken, profile, cb) => {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-);
+//   (accessToken, refreshToken, profile, cb) => {
+//     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// );
 
-module.exports = passportFacebook;
+// module.exports = passportFacebook;
