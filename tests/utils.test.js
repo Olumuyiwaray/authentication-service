0bf29a6044d689020ece19d60bae459a613cf26e
Samuel Jimoh <jimohsamolumuyiwa@gmail.com>
2024-04-29 10:35:27 +0100

    Fix tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,16 +1,26 @@
-const { checkPassword, createToken, hashPassword, } = require('../config/utils');
+const {
+  createToken,
+  hashPassword,
+  comparePassword,
+  genSalt
+} = require('../config/utils');
 
 test('createToken should return a string', async () => {
-    const response = await createToken(10)
-    expect(typeof response).toBe('string');
+  const response = await createToken(10);
+  expect(typeof response).toBe('string');
+});
+
+test('genSalt should generate salt', async () => {
+  const response = await genSalt();
+  expect(typeof response).toBe('string');
 });
 
 test('checkPassword should return a boolean', async () => {
-    const result = await checkPassword('john', 'etetetet');
-    expect(typeof result).toBe('boolean');
+  const result = await comparePassword('password', 'userPassword', 'userSalt');
+  expect(typeof result).toBe('boolean');
 });
 
 test('hashPassword should return a string', async () => {
-    const result = await hashPassword("goodluck");
-    expect(typeof result).toBe('string');
+  const result = await hashPassword('password', 'salt');
+  expect(typeof result).toBe('string');
 });
